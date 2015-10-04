a28853383011d77bb5e49dc32091b14dc370a413
Dillon <Dillon@SaraAndrsonsMBP.home>
2015-10-04 13:50:23 -0400

    can calculate what is touching on the first row and add a black square to it

--- game.py
+++ game.py
@@ -22,16 +22,10 @@ h = c_height/num_high
 # we're gonna use 5 colors that Tkinter supports
 #colors = ["red", "green", "blue", "purple", "orange"]
 # use less now to make work easier to understand
-colors = ["red", "green", "blue"]
+colors = ["red", "green"]
 
 # create an array of the colors so we can do work on it
 colors_array = [[random.randint(0, len(colors)-1) for x in range(num_wide)] for x in range(num_high)]
-
-# using the array, figure out how big the inital "block" is
-
-
-
-
 
 # draw the colors out
 # create columns
@@ -39,6 +33,35 @@ for col in range(num_high):
     # create a row across in that column
     for row in range(num_wide):
         c.create_rectangle(row*w, col*h, (row*w)+w, (col*h)+h, fill=colors[colors_array[col][row]])
+
+
+#print colors_array[0]
+
+# using the array, figure out how big the inital "block" is
+# go across row 0, seeing how far it's the same color
+# starting at 1, becasue the first one always has to match
+y_cntr = 1
+for y in range(num_wide-1):
+	# if the next is the same color, as this one, increase counter
+	if colors_array[0][y] == colors_array[0][y+1]:
+		y_cntr = y_cntr+1;
+	else:
+		# once they don't match, stop looking
+		break
+
+print y_cntr
+
+
+
+# draw a small black square on them so I know it's been counted - for debugging
+# create columns
+#for col in range(num_high):
+for col in range(1):
+    # create a row across in that column
+    for row in range(y_cntr):
+        c.create_rectangle(row*w, col*h, (row*w)+(w/2), (col*h)+(h/2), fill="black")
+
+
 
 mainloop()
 
